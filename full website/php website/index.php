3914823d2b5b65311ac2b2926025c2d5c253665f
Fazal <108507711+shaikcode@users.noreply.github.com>
2023-05-18 18:21:29 +0530

    Add files via upload

---
+++ full website/php website/index.php
@@ -0,0 +1,242 @@
+
+
+  
+  <?php
+
+  
+// echo "hellow world";
+// echo 5+98;
+// echo "my name is fazal";
+ 
+// echo "<br>";
+// echo "<br>";
+
+// echo "this is"; echo "fazal";
+
+// echo "<br>";
+
+// echo "hellow world";
+
+// echo "<br>";
+
+// $name ="fazal";
+// $nameb = "sohial";
+// $code = "vscode";
+// echo "my name is $name and i have a borther and his name is $nameb<br>";
+// echo "im coding php in $code<br>";
+
+// echo "<br>";
+// echo "<br>";
+
+//  $name ="fazal";
+//  echo "my name is $name";
+
+//  $number =200;
+//  echo "$number";      
+
+//  echo "<br>";
+//  echo "<br>";
+
+
+//  $names = array("fazal", "sohial", "sammer", "nasren");
+//  echo var_dump($names);
+//  echo "<br>";
+//  echo $names[0];
+//  echo "<br>";
+//  echo $names[1];
+//  echo "<br>";
+//  echo $names[2];
+//  echo "<br>"; 
+//  echo $names[3];
+//  echo "<br>";
+
+
+
+
+//  $names = array("fazal", "sohial", "sammer", "nasren");
+//  echo var_dump ($names); 
+
+
+    // this is    
+//    $name = "fazal is boy and he love coding";
+     
+//    echo "<br>";
+     
+//    echo "<br>";
+
+//    echo "the number of " . "of my is " . strlen($name);
+
+//    echo "<br>";
+
+//    echo "<br>";
+
+//    echo str_word_count($name);
+
+//     echo "<br>";
+
+//     echo "<br>";
+
+//     echo strrev($name);  
+     
+//      echo "<br>";
+
+//      echo "<br>";
+
+//     echo strpos($name, "is");
+
+//     echo "<br>";
+
+//     echo "<br>";
+
+//     echo str_replace("fazal", "sohial", $name);
+   
+//      echo "<br>";
+
+//      echo "<br>";
+
+//     echo str_repeat($name, 5);
+
+//      echo "<br>";
+
+//      echo "<br>";
+
+//     echo rtrim("<pre> this a good boy </pre>");
+
+//     echo "<br>";
+
+//     echo "<br>";
+
+//     // 09 video
+//     // operators in php
+   
+//     $a = 45;
+//     $b = 8;
+
+//     echo "for a + b, the result is" . ($a + $b) . "<br>";
+//     echo "for a - b, the result is" . ($a - $b) . "<br>";
+//     echo "for a * b, the result is" . ($a * $b) . "<br>";
+//     echo "for a / b, the result is" . ($a / $b) . "<br>";
+
+//       echo "<br>";
+// //10 video 
+
+// $a = 98;
+// $b = 79;
+
+// if($a > 78){
+//     echo "a is greater than 78";
+// }
+// else{
+//     echo "a is not greater than 78";
+// } 
+
+
+// $age = 19;
+
+// if ($age>18){
+//     echo "you can take driveing lances";
+// }
+
+// elseif ($age>13){
+//   echo "you can not take drveinf lances";
+// }
+
+// else{
+
+//      echo "you can not take at all you boy you just a boy";
+// }
+
+// my slef
+
+// $a =66;
+
+// if($a>65){
+
+//      echo "you can not drive a car because you are age is 65 and you are old<br><br>";
+
+// }
+
+// elseif($a>25){
+
+//       echo " you can drive <br><br>";
+
+// }
+
+// else{
+
+//      echo "you can not drive a car because you are small boy<br><br>";
+
+// }
+
+// echo "DONE";
+
+  // $age = "18";
+
+  // switch($age){
+
+  //    case 18:
+         
+  //      echo "you age is 18 so you can drive a car<br><br>";
+  //      break;
+
+  //   case 12:
+
+  //      echo "you age is 12 so you can not drive a car<br><br>";
+  //      break;
+      
+  //   case 17:
+  //      echo "you age is 17 so you can drive a brick in home<br><br>";
+  //      break;    
+    
+  //     default: 
+  //             echo "you can not drive a car<br><br>";
+  //             break;
+     
+  // } 
+
+ $number = 100;
+
+ switch($number){
+    case 100:
+      echo = 100 you have pass with very good number;
+        break;
+    case 50:
+      echo = 50 you have pass;
+       break;
+    case 30:
+      echo = 30 you have pass with less number;
+break;
+  default:
+    echo "you have no number"
+
+
+ }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+ 
